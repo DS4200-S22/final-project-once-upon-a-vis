9db338beb64c0eb11e4dcf8a5292cf8408f5cedf
veroaaguiar <aguiarortega.ve@husky.neu.edu>
2022-04-05 23:17:04 -0400

    nknk

--- js/book.js
+++ js/book.js
@@ -1,11 +1,24 @@
-// create svg element:
-var svg = d3.select("#rect").append("svg").attr("width", 800).attr("height", 200)
+
+var svg1 = d3.select("#rect").append("svg").attr("width", 800).attr("height", 600)
 
 // Add the path using this helper function
-svg.append('rect')
-  .attr('x', 10)
+svg1.append('rect')
+  .attr('x', 80)
   .attr('y', 120)
-  .attr('width', 600)
-  .attr('height', 40)
+  .attr('width', 400)
+  .attr('height', 600)
   .attr('stroke', 'black')
   .attr('fill', '#69a3b2');
+
+
+var svg2 = d3.select("#rect").append("svg").attr("width", 800).attr("height", 600)
+
+// Add the path using this helper function
+svg1.append('rect')
+  .attr('x', 40)
+  .attr('y', 160)
+  .attr('width', 40)
+  .attr('height', 400)
+  .attr('stroke', 'black')
+  .attr('fill', 'black');
+
