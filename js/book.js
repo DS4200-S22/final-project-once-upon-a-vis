bdb0718f7b3072899a24a09e19116a5e62c5af3e
veroaaguiar <aguiarortega.ve@husky.neu.edu>
2022-04-06 22:30:04 -0400

    4/6/22

--- js/book.js
+++ js/book.js
@@ -8,6 +8,7 @@ svg1.append('rect')
   .attr('width', 400)
   .attr('height', 600)
   .attr('stroke', 'black')
+  .attr('stroke-width', '2px')
   .attr('fill', '#69a3b2');
 
 
@@ -21,4 +22,39 @@ svg1.append('rect')
   .attr('height', 400)
   .attr('stroke', 'black')
   .attr('fill', 'black');
+
+
+  var svg3 = d3.select("#rect").append("svg").attr("width", 800).attr("height", 600)
+
+// Add the path using this helper function
+svg1.append('rect')
+  .attr('x', 40)
+  .attr('y', 120)
+  .attr('width', 40)
+  .attr('height', 40)
+  .attr('stroke', 'black')
+  .attr('fill', 'black');
+
+svg1.append('text')
+  .attr('x', 120)
+  .attr('y', 250)
+  .attr('stroke', 'black')
+  .style("font-size", 25)
+  .text("Example Title")
+
+svg1.append('text')
+  .attr('x', 120)
+  .attr('y', 450)
+  .attr('stroke', 'black')
+  .style("font-size", 25)
+  .text("Example Author")
+
+  var tri2 =  d3.symbol().type(d3.symbolDiamond).size(1800);
+    svg1.append("path")
+         .attr("d", tri2)
+         .attr("fill", "black")
+         .attr("stroke-width", "2px")
+         .attr("transform", "translate(60, 570) rotate(150)");
+
+
 
--- js/line_graph.js
+++ js/line_graph.js
@@ -3,11 +3,10 @@ const formatYear = d3.timeParse("%m/%d/%y");
 
 d3.csv('https://raw.githubusercontent.com/DS4200-S22/final-project-once-upon-a-vis/main/Data/top100_goodreads_cleaned.csv').then((data) => {
 
-    console.log(data);
 
 
 
-var date_and_value = data.map(function(d) {
+ const date_and_value = data.map(function(d) {
   return {
   	name: d.Title,
   	date: formatYear(d.Publ_Date),
@@ -15,7 +14,6 @@ var date_and_value = data.map(function(d) {
   }
 });
 
-console.log(date_and_value);
 
 
 
@@ -25,16 +23,16 @@ const margin = {top: 10, right: 30, bottom: 30, left: 60},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-    var maxDate = d3.max(date_and_value,function(d){return d.date; });
-    var minDate = d3.min(date_and_value,function(d){return d.date; });
-    var maxValue = d3.max(date_and_value,function(d){return d.value; });
+    const maxDate = d3.max(date_and_value,function(d){return d.date; });
+    const minDate = d3.min(date_and_value,function(d){return d.date; });
+    const maxValue = d3.max(date_and_value,function(d){return d.value; });
     console.log(maxDate);
     console.log(minDate);
     console.log(maxValue);
 
 
 //   // append the svg object to the body of the page
-var svg = d3.select("#line_graph")
+let svg = d3.select("#line_graph")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -57,11 +55,6 @@ var svg = d3.select("#line_graph")
       .range([ height, 0 ]);
     svg.append("g")
       .call(d3.axisLeft(y))
-
-// var cont = d3.rollup(date_and_value, v => d3.sum(v, d => d.value), d => d.date)
-// console.log(cont)
-
-// console.log(cont.value);
 
 
 
@@ -127,6 +120,10 @@ var svg = d3.select("#line_graph")
         .on("mouseover", mouseover)
         .on("mousemove", mousemove)
         .on("mouseleave", mouseleave)
+
+
+
+
 
 });
 
