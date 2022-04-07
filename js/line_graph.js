43ffc4c6587eac39b894b6ee4d520cddf2e69bf4
rgurn <97817513+rgurn@users.noreply.github.com>
2022-04-06 20:21:21 -0400

    Update line_graph.js

--- js/line_graph.js
+++ js/line_graph.js
@@ -3,7 +3,6 @@ const formatYear = d3.timeParse("%m/%d/%y");
 
 d3.csv('https://raw.githubusercontent.com/DS4200-S22/final-project-once-upon-a-vis/main/Data/top100_goodreads_cleaned.csv').then((data) => {
 
-    console.log(data);
 
 
 
@@ -15,7 +14,6 @@ var date_and_value = data.map(function(d) {
   }
 });
 
-console.log(date_and_value);
 
 
 
@@ -28,9 +26,6 @@ const margin = {top: 10, right: 30, bottom: 30, left: 60},
     var maxDate = d3.max(date_and_value,function(d){return d.date; });
     var minDate = d3.min(date_and_value,function(d){return d.date; });
     var maxValue = d3.max(date_and_value,function(d){return d.value; });
-    console.log(maxDate);
-    console.log(minDate);
-    console.log(maxValue);
 
 
 //   // append the svg object to the body of the page
@@ -59,9 +54,9 @@ var svg = d3.select("#line_graph")
       .call(d3.axisLeft(y))
 
 // var cont = d3.rollup(date_and_value, v => d3.sum(v, d => d.value), d => d.date)
-// console.log(cont)
+// console.(cont)
 
-// console.log(cont.value);
+// console.(cont.value);
 
 
 
