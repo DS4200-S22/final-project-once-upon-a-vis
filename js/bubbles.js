bfea78ab7dd81c170631cc45b0616fa9d5303a03
veroaaguiar <aguiarortega.ve@husky.neu.edu>
2022-04-10 13:05:22 -0400

    4/9/22

--- js/bubbles.js
+++ js/bubbles.js
@@ -1,24 +1,23 @@
-<script>
+
 
 // set the dimensions and margins of the graph
-var width = 450
-var height = 450
+const width = 450
+const height = 450
 
 // append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
+const svg = d3.select("#my_dataviz")
   .append("svg")
     .attr("width", 450)
     .attr("height", 450)
 
 // create dummy data -> just one element per circle
-var data = [{ "name": "A" }, { "name": "B" }, { "name": "C" }, { "name": "D" }, { "name": "E" }, { "name": "F" }, { "name": "G" }, { "name": "H" }]
+const data = [{ "name": "A" }, { "name": "B" }, { "name": "C" }, { "name": "D" }, { "name": "E" }, { "name": "F" }, { "name": "G" }, { "name": "H" }]
 
 // Initialize the circle: all located at the center of the svg area
-var node = svg.append("g")
+const node = svg.append("g")
   .selectAll("circle")
   .data(data)
-  .enter()
-  .append("circle")
+  .join("circle")
     .attr("r", 25)
     .attr("cx", width / 2)
     .attr("cy", height / 2)
@@ -28,7 +27,7 @@ var node = svg.append("g")
     .style("stroke-width", 4)
 
 // Features of the forces applied to the nodes:
-var simulation = d3.forceSimulation()
+const simulation = d3.forceSimulation()
     .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
     .force("charge", d3.forceManyBody().strength(0.5)) // Nodes are attracted one each other of value is > 0
     .force("collide", d3.forceCollide().strength(.01).radius(30).iterations(1)) // Force that avoids circle overlapping
@@ -39,8 +38,7 @@ simulation
     .nodes(data)
     .on("tick", function(d){
       node
-          .attr("cx", function(d){ return d.x; })
-          .attr("cy", function(d){ return d.y; })
+          .attr("cx", d => d.x)
+          .attr("cy", d => d.y)
     });
 
-</script>
